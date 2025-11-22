91ba567f9c4fdfc02250c939dcbe7e719b7beb19
Wellington Taureka <bruinnecessities@gmail.com>
2025-11-23 07:19:42 +1000

    Delete master_gene_pool/Python-Code-Harmonizer-main directory

--- master_gene_pool/Python-Code-Harmonizer-main/tests/user_validation/simple_data.py
+++
@@ -1,8 +0,0 @@
-class User:
-    def __init__(self, name, email):
-        self.name = name
-        self.email = email
-
-    def get_info(self):
-        """Returns user info."""
-        return f"{self.name} <{self.email}>"
--- master_gene_pool/Python-Code-Harmonizer-main/tests/user_validation/simple_hello.py
+++
@@ -1,8 +0,0 @@
-def say_hello(name):
-    """Prints a greeting."""
-    print(f"Hello, {name}!")
-
-
-def say_goodbye(name):
-    """Prints a farewell."""
-    print(f"Goodbye, {name}!")
